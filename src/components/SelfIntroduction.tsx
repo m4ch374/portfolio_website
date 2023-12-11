1acfb8fa391d13b2f446b8f8ced26f3343040c13
m4ch374 <spiner47@gmail.com>
2023-12-11 14:18:28 +0800

    dev(3d): added constraining div bound to 3d model

--- src/components/SelfIntroduction.tsx
+++ src/components/SelfIntroduction.tsx
@@ -5,10 +5,12 @@ import { Canvas } from "@react-three/fiber"
 const SelfIntroduction: React.FC = () => {
   return (
     <div className="flex h-[1000px] justify-center bg-[#b3734b] to-50% transition-colors delay-300 duration-200 ease-in dark:bg-[#1c0522]">
-      <div className="w-[90%] max-w-[800px] pt-20">
-        <Canvas>
-          <SpinningLaptop />
-        </Canvas>
+      <div className="flex w-[90%] max-w-[800px] flex-col pt-8">
+        <div className="h-[400px] w-[400px] place-self-center bg-white">
+          <Canvas>
+            <SpinningLaptop />
+          </Canvas>
+        </div>
       </div>
     </div>
   )
