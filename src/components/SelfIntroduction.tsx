384320a91d526adb8cc0562eab0bdc1a922f5922
m4ch374 <spiner47@gmail.com>
2023-12-12 23:21:15 +0800

    dev(laptop): added 3d model of laptop

--- src/components/SelfIntroduction.tsx
+++ src/components/SelfIntroduction.tsx
@@ -1,16 +1,11 @@
 import React from "react"
 import SpinningLaptop from "./SpinningLaptop"
-import { Canvas } from "@react-three/fiber"
 
 const SelfIntroduction: React.FC = () => {
   return (
     <div className="flex h-[1000px] justify-center bg-[#b3734b] to-50% transition-colors delay-300 duration-200 ease-in dark:bg-[#1c0522]">
       <div className="flex w-[90%] max-w-[800px] flex-col pt-8">
-        <div className="h-[400px] w-[400px] place-self-center bg-white">
-          <Canvas>
-            <SpinningLaptop />
-          </Canvas>
-        </div>
+        <SpinningLaptop />
       </div>
     </div>
   )
--- src/components/SpinningLaptop.tsx
+++ src/components/SpinningLaptop.tsx
@@ -1,21 +1,50 @@
 /* eslint-disable react/no-unknown-property */
 import { OrbitControls } from "@react-three/drei"
-import React, { useRef } from "react"
-import { Mesh } from "three"
+import { Canvas, Vector3, useLoader } from "@react-three/fiber"
+import React, { useEffect } from "react"
+// eslint-disable-next-line import/extensions
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+
+const CAMERA_POS: Vector3 = [10, 5, 8]
+const PERSPECTIVE_CAMERA_FOV = 40
+
+const LIGHT_INTENSITY = 1
+const LIGHT_COLOR = "white"
+
+const MODEL_INITIAL_POS: Vector3 = [-2, -2, 1]
+const MODEL_INITIAL_SCALE = 1
+const MODEL_INITIAL_Y_ROTATION = 270
 
 const SpinningLaptop: React.FC = () => {
-  const boxMesh = useRef<Mesh>(null!)
+  const loader = useLoader(GLTFLoader, "/laptop.glb")
+  useEffect(() => {
+    const model = loader.scene
+
+    model.rotation.y += MODEL_INITIAL_Y_ROTATION
+    model.position.set(
+      MODEL_INITIAL_POS[0],
+      MODEL_INITIAL_POS[1],
+      MODEL_INITIAL_POS[2],
+    )
+    model.scale.setScalar(MODEL_INITIAL_SCALE)
+  }, [loader])
 
   return (
-    <>
-      <ambientLight intensity={0.5} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh ref={boxMesh} castShadow receiveShadow>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial />
-      </mesh>
-      <OrbitControls autoRotate />
-    </>
+    <div className="flex h-[240px] w-[366px] items-center justify-center place-self-center bg-white/20 lg:h-[480px] lg:w-[700px]">
+      <Canvas
+        shadows
+        camera={{
+          position: CAMERA_POS,
+          fov: PERSPECTIVE_CAMERA_FOV,
+        }}
+      >
+        <ambientLight color={LIGHT_COLOR} intensity={LIGHT_INTENSITY} />
+
+        <primitive object={loader.scene} children-0-castShadow />
+
+        <OrbitControls autoRotate />
+      </Canvas>
+    </div>
   )
 }
 
