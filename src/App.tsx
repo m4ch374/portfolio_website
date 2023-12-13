db1fba0b5b8f909b43416a6cf5f6321e4781dbf7
m4ch374 <spiner47@gmail.com>
2023-12-13 21:47:30 +0800

    chore(reset): removed all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import FadeDevider from "components/FadeDevider"
-import NavBar from "components/NavBar"
-import ParallaxBanner from "components/ParallaxBanner"
-import SelfIntroduction from "components/SelfIntroduction"
 import React, { useState } from "react"
 import DarkTheme from "utils/DarkTheme"
 
-// comment
 const App: React.FC = () => {
   const themeController = useState(false)
 
   return (
     <DarkTheme.Provider value={themeController}>
-      <div className={`${!themeController[0] && "dark"}`}>
-        <NavBar />
-
-        <ParallaxBanner />
-
-        <FadeDevider />
-
-        <SelfIntroduction />
-      </div>
+      <div></div>
     </DarkTheme.Provider>
   )
 }
